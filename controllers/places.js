c7bc0eb981186606544d206a46917a7a70e944ec
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-10-28 19:43:49 -0500

    places commit

---
+++ controllers/places.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+// More code here in a moment
+
+module.exports = router
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
+
+module.exports = router

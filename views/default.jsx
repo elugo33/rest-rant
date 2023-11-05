4084bb8e7a3828a5935be0b454bd6df96e3c043e
mac <mac@macs-iMac.attlocal.net>
2023-11-04 20:26:20 -0500

    adding home page view

---
+++ views/default.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+
+function Def (html) {
+    function Def (html) {
+        return (
+            <html>
+                <head>
+                    <title>Title</title>
+                </head>
+                <body>
+                    {html.children}
+                </body>
+            </html>
+        )
+      }
+      
+}
+
+module.exports = Def
+

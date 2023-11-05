a56ebdf8f7578b96e9614917b3f81ab9528b720c
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-11-05 10:46:40 -0600

    show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -40,10 +40,14 @@ app.get('/', (req, res) => {
       </Def>
     )
   }
-  let placesFormatted = data.places.map((place) => {
+  let placesFormatted = data.places.map((place, index) => {
     return (
       <div className="col-sm-6">
-        <h2>{place.name}</h2>
+        <h2>
+          <a href={`/places/${index}`} >
+            {place.name}
+          </a>
+        </h2>
         <p className="text-center">
           {place.cuisines}
         </p>
@@ -54,6 +58,7 @@ app.get('/', (req, res) => {
       </div>
     )
   })
+  
   
   
   name: 'H-Thai-ML',

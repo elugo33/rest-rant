ddcbde3e721df398691108a68aae67074614b7dd
mac <mac@macs-iMac.attlocal.net>
2023-11-04 21:21:02 -0500

    adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,6 +8,8 @@ function error404 () {
               <main>
                   <h1>404: PAGE NOT FOUND</h1>
                   <p>Oops, sorry, we can't find this page!</p>
+
+        Comic featured on  <a href="https://www.npr.org/2019/12/15/787618154/imagine-pleasant-nonsense-with-strange-planet-creator-nathan-pyle">comic by Nathan Pyle</a> on <a href="https://media.npr.org/assets/img/2019/12/12/absorb_custom-d6da914ba9ab993edf85c72e530d2c59f171b131-s800-c85.webp">Unsplash</a>
               </main>
           </Def>
         )
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -23,17 +23,52 @@ app.get('/', (req, res) => {
   function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
+        <div className="col-sm-6">
           <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
+          <img src={place.pic} alt={place.name} />
         </div>
       )
     })
     return (
       <Def>
           <main>
-              <h1>PLACES INDEX PAGE</h1>
-              {placesFormatted}
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
+                {placesFormatted}
+              </div>
+          </main>
+      </Def>
+    )
+  }
+  let placesFormatted = data.places.map((place) => {
+    return (
+      <div className="col-sm-6">
+        <h2>{place.name}</h2>
+        <p className="text-center">
+          {place.cuisines}
+        </p>
+        <img src={place.pic} alt={place.name} />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    )
+  })
+  
+  
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: '/images/h-thai-ml-tables.png'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg'
+}]
+}
           </main>
       </Def>
   )

5e90f31565ad6699a2ce8f0e28336e12e57286ba
mac <mac@macs-iMac.attlocal.net>
2023-11-04 21:45:13 -0500

    new places can be added

--- views/home.jsx
+++ views/home.jsx
@@ -2,20 +2,21 @@ const React = require('react')
 const Def = require('./default')
 
 function home () {
-    
-        return (
-          <Def>
-              <main>
-                  <h1>HOME</h1>
-                  <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
-
-              </main>
-          </Def>
-        )
-      }
-      
+    return (
+        <Def>
+            <main>
+            <main>
+    <h1>HOME</h1>
+    <div>
+      <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+      <div>
+        Photo by <a href="https://unsplash.com/@vdphotography">VD Photography</a> on <a href="https://unsplash.com/photos/a-close-up-of-two-glasses-of-food-on-a-table-bT-eRvDBrZQ">Unsplash</a>
+      </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
 
 
 module.exports = home

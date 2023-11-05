2308b9cd38bed89a003a1a19332d88ff446cde9b
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-11-05 11:57:52 -0600

    debugged

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -30,6 +30,24 @@ function show (data) {
     Delete
   </button>
 </form> 
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
+  
+    }
+  })
+  
      
 
 

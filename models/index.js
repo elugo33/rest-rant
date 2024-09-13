5f4aacf484df42aee56bfdd70298c4283a5fde55
Eli Ugolino <eli.ugolino@gmail.com>
2024-09-12 19:52:45 -0500

    set-up

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+module.exports.Place = require('./places')
